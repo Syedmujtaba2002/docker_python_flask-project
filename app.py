3572edd4bd00fd2aca901f6f6d7b6fd0881c0235
Syedmujtaba2002 <syedmujtabahussaini03@gmail.com>
2025-07-30 15:43:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask sample application welcome to better"
+    return "Flask sample application welcome to better syed"
 
 
 if __name__ == "__main__":
